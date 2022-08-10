1eb10c0479fca76777fea3d102bb01cbb8cbf31e
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-08-10 17:13:15 -0300

    Adicionando CRUD

--- src/index.js
+++ src/index.js
@@ -6,8 +6,31 @@ const app = express()
 /*request é tudo aquilo que a gente recebe da requisição, 
 response é aquilo que vamos retornar
 */
-app.get("/", (request, response) => { 
-    return response.json({message: "Hello World!"});
-})
 
+/*
+    GET - Buscar uma informação dentro do servidor
+    POST - Inserir uma informação no servidor
+    PUT - Alterar uma informação no servidor
+    PATCH - Alterar uma informação específica
+    DELETE - Deletar uma informação no servidor
+*/
+app.get("/courses", (request, response) => { 
+    return response.json(["Curso 1, Curso 2, Curso 3"])
+});
+
+app.post("/courses", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) =>{
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) =>{
+    return response.json(["Curso 6",  "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) =>{
+    return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
 app.listen(3030);
