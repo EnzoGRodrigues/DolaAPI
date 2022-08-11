21a35488b55fad446eee245b99b02bc1d25c69d6
Enzo Rodrigues <enzorodrigues272@gmail.com>
2022-08-10 22:37:23 -0300

    Implementando verificação de CPF existente

--- src/index.js
+++ src/index.js
@@ -37,11 +37,17 @@ response é aquilo que vamos retornar
 
 app.post("/account", (request, response) =>{
     const {cpf, name} = request.body;
-    const id = uuidv4();
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
     return response.status(201).send();
